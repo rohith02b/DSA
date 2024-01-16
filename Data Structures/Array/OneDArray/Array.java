1650faf55a2e50d023ea64a5e0cf30967a8b5245
rohith02b <rohith20b@gmail.com>
2024-01-16 20:55:36 +0530

    fix : 1D array

--- Data Structures/Array/OneDArray/Array.java
+++ Data Structures/Array/OneDArray/Array.java
@@ -1,13 +1,11 @@
 import java.util.Arrays;
 
 class Array {
-  int arr[] = null;
+  int[] arr = null;
 
   public Array(int size) {
     arr = new int[size];
-    for (int i = 0; i < arr.length; i++) {
-      arr[i] = Integer.MIN_VALUE;
-    }
+      Arrays.fill(arr, Integer.MIN_VALUE);
   }
 
   public void insert(int index, int value) {
@@ -28,11 +26,60 @@ class Array {
       if (isArrayEmpty) {
         System.out.println("The array is empty");
         return;
-      } 
+      }
+      if(arr[index] == Integer.MIN_VALUE)
+      {
+        throw new Exception("");
+      }
       System.out.println("The element at index " + index + " is " + arr[index]);
     } catch (Exception e) {
         System.out.println("Invalid index, index out of bounds of the array");
     } 
+  }
+
+  public int findElement(int element)
+  {
+    int flag=0;
+    try{
+      for (int i=0;i<arr.length;i++)
+      {
+        if(arr[i] == element) {
+          flag=1;
+          return i;
+        }
+      }
+
+      if(flag == 0)
+        throw new Exception("Element not found");
+
+      return -1;
+    }
+    catch(Exception e) {
+      return -1;
+    }
+  }
+
+  public void deleteElementViaIndex(int index){
+    try {
+      if(arr[index] == Integer.MIN_VALUE) {
+        System.out.println("Invalid index");
+        return;
+      }
+      System.out.print("Removed element : " + arr[index]);
+      arr[index] = Integer.MIN_VALUE;
+    }
+    catch(Exception e) {
+      System.out.println("Invalid index");
+    }
+  }
+
+  public void deleteElementViaValue(int value) {
+      int index = findElement(value);
+      if(index == -1) {
+        System.out.println("Element not found");
+        return;
+      }
+      arr[index] = Integer.MIN_VALUE;
   }
 
   public void display() {
@@ -42,11 +89,11 @@ class Array {
         throw new Exception("Array is empty");
       }
       System.out.print("[ ");
-      for (int i = 0; i < arr.length; i++) {
-        if (arr[i] != Integer.MIN_VALUE) {
-          System.out.print(arr[i] + " ");
+        for (int j : arr) {
+            if (j != Integer.MIN_VALUE) {
+                System.out.print(j + " ");
+            }
         }
-      }
       System.out.print("]");
     } catch (Exception e) {
       System.out.println(e);
@@ -54,11 +101,11 @@ class Array {
   }
 
   private Boolean getArrStatus() {
-    for (int i = 0; i < arr.length; i++) {
-      if (arr[i] != Integer.MIN_VALUE) {
-        return false;
+      for (int j : arr) {
+          if (j != Integer.MIN_VALUE) {
+              return false;
+          }
       }
-    }
     return true;
   }
 }
--- Data Structures/Array/OneDArray/Main.java
+++ Data Structures/Array/OneDArray/Main.java
@@ -1,23 +1,68 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
     Array a = new Array(5);
 
-    // Empty array case
-    a.display();
-    a.getElementAtIndex(1);
+    int choice;
+    do {
+      System.out.println("\nMenu:");
+      System.out.println("1. Display array");
+      System.out.println("2. Get element at index");
+      System.out.println("3. Insert element");
+      System.out.println("4. Find element");
+      System.out.println("5. Delete element by index");
+      System.out.println("6. Delete element by value");
+      System.out.println("0. Exit");
 
-    a.insert(1, 100);
-    a.insert(2, 200);
-    a.insert(3, 300);
+      System.out.print("Enter your choice: ");
+      choice = scanner.nextInt();
 
-    // Occupied case
-    a.insert(1, 100);
+      switch (choice) {
+        case 1:
+          a.display();
+          break;
+        case 2:
+          System.out.print("Enter index: ");
+          int index = scanner.nextInt();
+          a.getElementAtIndex(index);
+          break;
+        case 3:
+          System.out.print("Enter index: ");
+          int insertIndex = scanner.nextInt();
+          System.out.print("Enter value: ");
+          int insertValue = scanner.nextInt();
+          a.insert(insertIndex, insertValue);
+          break;
+        case 4:
+          System.out.print("Enter element to find: ");
+          int elementToFind = scanner.nextInt();
+          int foundIndex = a.findElement(elementToFind);
+          if (foundIndex != -1)
+            System.out.println("Found element at index: " + foundIndex);
+          else
+            System.out.println("Element not found.");
+          break;
+        case 5:
+          System.out.print("Enter index to delete: ");
+          int deleteIndex = scanner.nextInt();
+          a.deleteElementViaIndex(deleteIndex);
+          break;
+        case 6:
+          System.out.print("Enter value to delete: ");
+          int deleteValue = scanner.nextInt();
+          a.deleteElementViaValue(deleteValue);
+          break;
+        case 0:
+          System.out.println("Exiting program. Goodbye!");
+          break;
+        default:
+          System.out.println("Invalid choice. Please enter a valid option.");
+          break;
+      }
+    } while (choice != 0);
 
-    a.getElementAtIndex(1);
-
-    // out of bounds case
-    a.getElementAtIndex(5);
-
-    a.display();
+    scanner.close();
   }
 }
